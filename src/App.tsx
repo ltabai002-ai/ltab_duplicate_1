239118f8b413afff9ae199fdecd6f058446ac786
kash2021-hobby <kashyapnandan2021@gmail.com>
2025-08-19 17:59:11 +0530

    Fix App.tsx syntax error

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-@@ .. @@
- import React from 'react';
- import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-+import AiGallery from './pages/AiGallery';
- 
- function App() {
-   return (
-     <Router>
-       <div className="App">
-+        </nav>
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AiGallery from './pages/AiGallery';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
           <Route path="/" element={<AiGallery />} />
-           <Route path="/" element={<div className="p-8"><h1 className="text-2xl font-bold">Welcome to the App</h1></div>} />
-+          <Route path="/ai-gallery" element={<AiGallery />} />
-         </Routes>
-       </div>
-     </Router>
-   )
- }
+          <Route path="/ai-gallery" element={<AiGallery />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App;
