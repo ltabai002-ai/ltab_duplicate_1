37c21259f1ff443ddce9477deb0bfdcdfd42ebe2
kash2021-hobby <kashyapnandan2021@gmail.com>
2025-08-19 17:55:39 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@
 +            <div className="flex justify-between h-16">
 +              <div className="flex items-center space-x-8">
 +                <a 
+   )
+ }
 +                  href="/" 
 +                  className="text-gray-900 hover:text-gray-700 px-3 py-2 text-sm font-medium transition duration-200"
 +                >
