ac25966ed1ed4301a18417902cfb0a63066a5f38
kash2021-hobby <kashyapnandan2021@gmail.com>
2025-09-03 10:51:36 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AiGallery from './pages/AiGallery';
 
 function App() {
-  return <AiGallery />;
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<AiGallery />} />
+          <Route path="/ai-gallery" element={<AiGallery />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
